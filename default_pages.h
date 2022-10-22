11d5f1974b3dc4ccae96a69f719273a3c8f7f48a
Louis Sven Goulet <louis@era.co>
2022-10-22 17:09:15 -0400

    moved hardcoded html pages into default_pages.h

---
+++ default_pages.h
@@ -0,0 +1,67 @@
+#ifndef DEFAULT_PAGES_H
+#define DEFAULT_PAGES_H 1
+
+#include <stddef.h>
+
+const char I_AM_A_TEAPOT[] = (
+    "<!DOCTYPE html>"
+        "<html>"
+            "<head>"
+                "<title>teapot</title>"
+            "</head>"
+            "<body>"
+                "<h1 style=\"text-align: center;\">I'm a teapot</h1>"
+                "<h1 style=\"text-align: center;\">"
+                    "<code><strong>{(&macr;)/&acute;</strong></code>"
+                "</h1>"
+                "<h3 style=\"text-align: center;\">"
+                    "Will you have a cup of Tea?"
+                "</h3>"
+            "</body>"
+        "</html>"
+);
+const size_t I_AM_A_TEAPOT_LEN = sizeof(I_AM_A_TEAPOT);
+
+const char NOT_FOUND_PAGE[] = (
+    "<!DOCTYPE html>"
+        "<html>"
+            "<head>"
+                "<title>404</title>"
+            "</head>"
+            "<body>"
+                "<h1>NOT_FOUND</h1>"
+                "<p>The resource asked for could not be found</p>"
+            "</body>"
+        "</html>"
+);
+const size_t NOT_FOUND_PAGE_LEN = sizeof(NOT_FOUND_PAGE);
+
+const char UNIMPLEMENTED_PAGE[] = (
+    "<!DOCTYPE html>"
+        "<html>"
+            "<head>"
+                "<title>405</title>"
+            "</head>"
+            "<body>"
+                "<h1>UNIMPLEMENTED</h1>"
+                "<p>this server only implements http GET</p>"
+            "</body>"
+        "</html>"
+);
+const size_t UNIMPLEMENTED_PAGE_LEN = sizeof(UNIMPLEMENTED_PAGE);
+
+const char SERVER_ERROR_PAGE[] = (
+    "<!DOCTYPE html>"
+        "<html>"
+            "<head>"
+                "<title>500</title>"
+            "</head>"
+            "<body>"
+                "<h1>SERVER ERROR</h1>"
+                "<p>server error check the server's logs for more info</p>"
+            "</body>"
+        "</html>"
+);
+const size_t SERVER_ERROR_PAGE_LEN = sizeof(SERVER_ERROR_PAGE);
+
+#endif
